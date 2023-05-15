88964c019a9df2d55f58189ba93dbc2b1901854b
Haojin Li <hl420@duke.edu>
2023-05-15 16:45:03 -0400

    Set up mongo db

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
