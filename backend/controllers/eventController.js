b088b916939482f3898cdde18ffd2dde109725a1
Haojin Li <hl420@duke.edu>
2023-05-16 15:41:18 -0400

    Setup

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,34 @@
+// @desc  Get all events
+// @route GET /api/events
+// @access Private
+const getEvents = (req, res) => {
+    res.status(200).json({message: "Get events"});
+}
+
+// @desc  Create a new event
+// @route POST /api/events
+// @access Private
+const createEvent = (req, res) => {
+    res.status(200).json({message: "Create a new event"});
+}
+
+// @desc  Update an event
+// @route PUT /api/events/:id
+// @access Private
+const updateEvent = (req, res) => {
+    res.status(200).json({message: `Update event ${req.params.id}`});
+}
+
+// @desc  Delete an event
+// @route DELETE /api/events/:id
+// @access Private
+const deleteEvent = (req, res) => {
+    res.status(200).json({message: `Delete event ${req.params.id}`});
+}
+
+module.exports = {
+    getEvents,
+    createEvent,
+    updateEvent,
+    deleteEvent
+}
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/events', require('./routes/eventRoutes'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+    }
+);
