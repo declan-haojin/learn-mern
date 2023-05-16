82c16222772e08171f055bd9c626b830f18cf3a7
Haojin Li <hl420@duke.edu>
2023-05-16 12:02:08 -0400

    Finish view setup

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const passport = require('passport');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 // Connect to database
 connectDB();
@@ -18,8 +23,14 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
