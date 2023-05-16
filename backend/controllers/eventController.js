6044e0e06bd98b72c994e7220e32698d9a8ac272
Haojin Li <hl420@duke.edu>
2023-05-16 16:04:12 -0400

    Setup database

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc  Get all events
 // @route GET /api/events
 // @access Private
-const getEvents = (req, res) => {
+const getEvents = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Get events"});
-}
+});
 
 // @desc  Create a new event
 // @route POST /api/events
 // @access Private
-const createEvent = (req, res) => {
+const createEvent = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(404);
         throw new Error("Event text is required");
     }
     res.status(200).json({message: "Create a new event"});
-}
+});
 
 // @desc  Update an event
 // @route PUT /api/events/:id
 // @access Private
-const updateEvent = (req, res) => {
+const updateEvent = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update event ${req.params.id}`});
-}
+});
 
 // @desc  Delete an event
 // @route DELETE /api/events/:id
 // @access Private
-const deleteEvent = (req, res) => {
+const deleteEvent = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete event ${req.params.id}`});
-}
+});
 
 module.exports = {
     getEvents,
