95cfb9e6b370559cb7d06ab72fc5e1ac57ca9bb3
Haojin Li <hl420@duke.edu>
2023-05-16 15:43:14 -0400

    Basic controller for the application.

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/eventController');
 
-router.get('/', getEvents);
-
-router.post('/', createEvent);
-
-router.put('/:id', updateEvent);
-
-router.delete('/:id', deleteEvent);
+router.route('/').get(getEvents).post(createEvent);
+router.route('/:id').put(updateEvent).delete(deleteEvent);
 
 module.exports = router;
-
-
