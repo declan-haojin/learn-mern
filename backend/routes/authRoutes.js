846bfbb20e74d9d336cc3e5cda2bfe57f1019653
Haojin Li <hl420@duke.edu>
2023-05-17 01:31:32 -0400

    Add JWT token when sending back google login info

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 // @desc    Auth with Google
 // @route   GET /auth/google
+// @access  Public
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
+// @access  Public
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.json({ message: "Login successful", user: req.user });
+    res.json({
+        message: "Login successful",
+        user: req.user,
+        token: generateToken(req.user._id),
+    });
 });
 
 // @desc    Logout user
 // @route   GET /auth/logout
-router.get('/logout', (req, res) => {
+// @access  Private
+router.get('/logout', protect, (req, res) => {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.redirect('/');
     });
 });
+
+// Generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+}
+
 
 
 module.exports = router;
