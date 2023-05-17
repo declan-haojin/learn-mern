5be6aa9582ea046f2b9ee5b4ca701eaeb9b80cef
Haojin Li <hl420@duke.edu>
2023-05-16 21:36:31 -0400

    Finish google auth

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    // res.json({ message: "Login successful" });
+    res.json({ message: "Login successful", user: req.user });
+    // res.redirect('/');
+});
+
+// @desc    Logout user
+// @route   GET /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,46 @@ const dotenv = require('dotenv').config();
 const errorHandler = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 connectDB();
+
+require('./config/passport')(passport);
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use('/api/events', require('./routes/eventRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+// Sessions
+app.use(session({
+    secret: 'root causes',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/api/events', require('./routes/eventRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
 
 
 app.listen(port, () => {
